d6298529f77c1a59856e0942d1aa59047589c8fb
Juan Carlos Ramirez <the_passenger89@hotmail.com>
2020-01-14 08:13:11 -0600

    Update image_to_64jsonvisionrequest.py

--- image_to_64jsonvisionrequest.py
+++ image_to_64jsonvisionrequest.py
@@ -4,28 +4,65 @@ Created on Thu Jan  9 10:28:05 2020
 
 @author: cenic
 """
+from requests.auth import HTTPBasicAuth
 import base64
 import json
+import requests
+import glob
+import os
+
+
 def encode_image(image):
-  image_content = image.read()
-  return base64.b64encode(image_content)
-with open("donuts.jpeg", 'rb') as image:
-    encoded=encode_image(image)
-content = encoded.decode('ascii')
-jsonstr = {
-  "requests":[
-    {
-      "image":{
-        "content": content
-      },
-      "features": [
-        {
-          "type":"LABEL_DETECTION",
-          "maxResults":1
+    image_content = image.read()
+    return base64.b64encode(image_content)
+
+
+def createjson(filename):
+    with open(filename, 'rb') as image:
+        encoded = encode_image(image)
+    content = encoded.decode('ascii')
+    jsonstr = {"requests": [
+            {"image": {"content": content},
+             "features": [{"type": "LABEL_DETECTION",
+                           "maxResults": 10
+                           }]
+             }
+            ]
         }
-      ]
-    }
-  ]
-}
-with open('request.json', 'w') as f:
-    json.dump(jsonstr,f)
+    return jsonstr
+def makeVisionRequestforImg(jsonobj, apikey):
+    authorization = HTTPBasicAuth("key", apikey) 
+    url = "https://vision.googleapis.com/v1/images:annotate?key="
+    flag_retry = "y"
+    while(flag_retry=="y"):
+        flag_retry=False
+        try:
+            r = requests.post(url + apikey,
+                      json=jsonobj,
+                      auth=authorization)
+        except requests.exceptions.RequestException as e:
+            r = e
+            # catastrophic error. bail.
+            print(e) 
+            input("continue? (y/n)")
+    return r
+
+def main(apikey, alreadyprocessed):
+    lineList = [line.rstrip('\n') for line in open(alreadyprocessed)]
+    filelist = glob.glob("Images/*")
+    filelist =[x for x in filelist if x not in lineList]
+
+    jsonlist=[]
+    for filename in filelist:
+        print(filename)
+        jsonobj = createjson(filename)
+        filepref = os.path.splitext(filename)[0][7:]
+        with open('request/{}_req.json'.format(filepref), 'w') as f:
+            json.dump(jsonobj, f)
+        r= makeVisionRequestforImg(jsonobj, apikey)
+        with open('response/{}_resp.json'.format(filepref), 'w') as f:
+            json.dump(r.json(), f)
+            jsonlist.append(r.json())
+    return jsonlist
+        
+
